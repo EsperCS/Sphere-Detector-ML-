72e7b3986813c453bda7261639017019e8d52d2b
unknown <lnguyenthiennhan@gmail.com>
2025-08-22 16:11:26 +0700

    Use this to generate data

--- randomize program.py
+++ randomize program.py
@@ -8,7 +8,6 @@ z0 = random.uniform(1, 10)
 r = random.uniform(20, 30)
 
 for _ in range(50):
-    # sinh góc
     theta = random.uniform(0, math.pi)
     phi = random.uniform(0, 2*math.pi)
     x = r * math.sin(theta) * math.cos(phi) + x0
